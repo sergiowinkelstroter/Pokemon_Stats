c5ec49a17c59e95c7bffd5055e19bd316dd8b1ef
Sergio <winksousa0@gmail.com>
2022-06-15 17:08:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [pokemonName, setpokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
+
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => {
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defense: response.data.stats[2].base_stat,
+          type: response.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      });
+  };
+
+  return (
+    <div className="App">
+      <div className="TitleSection">
+        <h1>Pokemon Stats</h1>
+        <input
+          type="text"
+          className="InputPokemons"
+          onChange={(e) => {
+            setpokemonName(e.target.value);
+          }}
+        />
+        <button className="Bnt-Pokemon" onClick={searchPokemon}>
+          Search Pokemon
+        </button>
+      </div>
+      <div className="DisplaySection">
+        {!pokemonChosen ? (
+          <h1> Please choose a Pokemon</h1>
+        ) : (
+          <>
+            <h1> {pokemon.name.toUpperCase()}</h1>
+            <img src={pokemon.img} />
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type: {pokemon.type}</h3>
+            <h4>Hp: {pokemon.hp}</h4>
+            <h4>Attack: {pokemon.attack}</h4>
+            <h4>Defense: {pokemon.defense}</h4>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
